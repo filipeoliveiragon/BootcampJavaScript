66a0274bbd64b30b8e1f2ed6c881a119ce11d620
filipeoliveiragon <filipeoliveiragon@gmail.com>
2022-02-27 14:42:31 -0300

    ArquivoNovo

--- HTMLeCSS/arrays-1.js
+++ HTMLeCSS/arrays-1.js
@@ -24,35 +24,35 @@ const persons = [
 ];
 
 // //Retonar a quantidade de intes
-// console.log('Items:', persons.length);
+console.log('Items:', persons.length);
 
 // //Verificar tipo
-// console.log('Avariável person é um array:', Array.isArray(persons));
+console.log('Avariável person é um array:', Array.isArray(persons));
 
 // //Iterar os itens do array
-// persons.forEach(person => {
-//     console.log(`Nome: ${person.name}`)
-// });
+persons.forEach(person => {
+    console.log(`Nome: ${person.name}`)
+});
 
 //Filtrar array
-// const mens = persons.filter(person => person.gender === gender.MAN);
-// console.log('\nNova lista apenas com homens:', mens);
+const mens = persons.filter(person => person.gender === gender.MAN);
+console.log('\nNova lista apenas com homens:', mens);
 
 // //Retornar um novo
-// const personWithCourse = persons.map(person => {
-//     person.course = 'Introdução ao JS';
-//     return person;
-// });
+const personWithCourse = persons.map(person => {
+    person.course = 'Introdução ao JS';
+    return person;
+});
 
 // console.log('\nPessoas com a adiçao de course:' , personWithCourse);
 
 // //Transforamr um array em outro tipo
-// const totalAge = persons.reduce((age, person) => {
-//     age += person.age;
-//     return age;
-// }, 0 );
+const totalAge = persons.reduce((age, person) => {
+    age += person.age;
+    return age;
+}, 0 );
 
-// console.log('\nSoma de idade das pessoas', totalAge);
+console.log('\nSoma de idade das pessoas', totalAge);
 
 //Juntando operações
 const totalEvenAges = persons
@@ -63,3 +63,5 @@ const totalEvenAges = persons
 }, 0);
 
 console.log('\nSoma de idades das peassoas que possuem idade par: ', totalEvenAges);
+
+
